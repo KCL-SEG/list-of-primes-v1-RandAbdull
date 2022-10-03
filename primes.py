4f98e9c5704682bfc0265bf988f354ed08da01a9
Rand Albahusain <114617321+RandAbdull@users.noreply.github.com>
2022-10-03 16:13:12 +0100

    Implement a function that generate n prime numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,36 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
-    list = []
-    return list
+
+
+inputNum = int(raw_input('How many primes to print?  '))
+count = 0
+potentialprime = 2
+primesList = []
+
+
+def primetest(potentialprime):
+    divisor = 2
+    
+    while divisor <= potentialprime:
+        if potentialprime == 2:
+            return True
+        elif potentialprime % divisor == 0:
+            return False
+        while potentialprime % divisor != 0:
+            if potentialprime - divisor > 1:
+                divisor += 1
+            else:
+                return True
+            
+# generate prime numbers until count reaches the user input number            
+while count < inputNum:
+    if primetest(potentialprime) == True:
+        # add the prime number to the list
+        primesList.append(potentialprime)
+        count += 1
+    potentialprime += 1
+    
+print(primesList)
+
+
